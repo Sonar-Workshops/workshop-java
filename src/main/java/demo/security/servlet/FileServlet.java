f1877551105713feedd155d2b7317f0cd5c0a15e
Brian Cipollone <40029762+brian-cipollone-sonarsource@users.noreply.github.com>
2026-02-18 06:15:59 -0600

    Update FileServlet.java

--- src/main/java/demo/security/servlet/FileServlet.java
+++ src/main/java/demo/security/servlet/FileServlet.java
@@ -14,7 +14,6 @@ public class FileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String data = request.getParameter("data");
-        String var = "";
         Utils.deleteFile(data);
     }
 }
